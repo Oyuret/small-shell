c192e1079f8ed0851e20eed6e190f395eaec9e9d
Yuri Stange <yuris@kth.se>
2014-05-01 15:03:20 +0200

    Added parseArgs

--- shell.c
+++ shell.c
@@ -8,22 +8,45 @@
 
 #define MAXLEN 70
 
-/*Print out the possible error*/
-void err(char* msg)
-{
+/**
+ * Print out the possible error
+ */
+void err(char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-int main (int argc, char * argv[], char * envp[])
-{
+/**
+ * Parses the arguments given. Argv needs to be null-terminated
+ * Terminates the copy with NULL
+ * Returns 0 if it worked
+ */
+int parseArgs(char * argv, char * copy[]) {
+    char * token = strtok(argv, " ");
 
-    int child, i;
+    int i=0;
+    while(token!=NULL) {
+        copy[i]=token;
+        token = strtok(NULL, " ");
+        i++;
+
+        /* If we reach 7 then argv wasn't terminated*/
+        if(i>8) err("parseArgs: argv lacks null-termination");
+    }
+
+    copy[i] = NULL;
+
+    /* It worked! */
+    return 0;
+}
+
+int main (int argc, char * argv[], char * envp[]) {
+
+    int child;
     char input[MAXLEN];
     char * input_cmd[7];
 
-    while (1)
-    {
+    while (1) {
 
         /* Utskrift */
         printf("small-shell> ");
@@ -33,31 +56,23 @@ int main (int argc, char * argv[], char * envp[])
         input[strlen(input)-1] = '\0';
 
         /* Exit ?*/
-        if (strcmp(input, "exit") == 0)
-        {
+        if (strcmp(input, "exit") == 0) {
             break;
         }
 
-        char * token = strtok(input, " ");
-        i=0;
-        while(token!=NULL) {
-            input_cmd[i]=token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        input_cmd[i] = NULL;
+        /* Parse the inputs */
+        parseArgs(input, input_cmd);
 
+        /* Fork to child process */
         child = fork();
 
+        /* Child or main */
         if (child == -1)
             err("Failed creating child\n");
 
-        if (child == 0)
-        {
-
-            if (execvp(input_cmd[0], input_cmd) == -1)
-            {
-                err("Error");
+        if (child == 0) {
+            if (execvp(input_cmd[0], input_cmd) == -1) {
+                err("Couldn't run the given inputs");
             }
         }
         waitpid(child, NULL, 0);
